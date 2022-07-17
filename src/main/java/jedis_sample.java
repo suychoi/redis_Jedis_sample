0282499426f9a014f72b443e68fed6fe58e7ea7c
suychoi <jack96318@hanmail.net>
2022-07-17 22:08:55 +0900

    initial commit

---
+++ src/main/java/jedis_sample.java
@@ -0,0 +1,25 @@
+import redis.clients.jedis.*;
+
+public class jedis_sample {
+    public static void main(String[] args){
+
+
+
+
+
+
+
+
+//        M/R
+//        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
+//
+//        JedisPool pool = new JedisPool(jedisPoolConfig, "172.30.5.186", 6379, 2000, "foobared");
+//        Jedis jedis = pool.getResource();
+//
+//        jedis.set("test", "test_val");
+//        System.out.println(jedis.get("test"));
+
+    }
+}
+
+
